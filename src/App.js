b02347d0b07f97c279d2fa69c2b9e0e3c17b37b1
Md Anas Sabah <mdanassabah@gmail.com>
2022-06-21 21:18:17 +0530

    ADD: Section-UseState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+import UseState from "./hooks/UseState";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Let's Play With React Hooks🚀</h1>
+      <Router>
+        <NavUnlisted>
+          <Link to="/state">
+            <button>useState Hook</button>
+          </Link>
+          <Link to="/state">
+            <button>useState Hook</button>
+          </Link>
+          <Link to="/state">
+            <button>useState Hook</button>
+          </Link>
+          <Link to="/state">
+            <button>useState Hook</button>
+          </Link>
+          <Link to="/state">
+            <button>useState Hook</button>
+          </Link>
+          <Link to="/state">
+            <button>useState Hook</button>
+          </Link>
+          <Link to="/state">
+            <button>useState Hook</button>
+          </Link>
+        </NavUnlisted>
+        <Routes>
+          <Route path="/state" element={<UseState />} />
+        </Routes>
+      </Router>
+      <Last>@2022 Copyright:Md Anas Sabah</Last>
     </div>
   );
 }
 
 export default App;
+
+const NavUnlisted = styled.nav`
+  display: flex;
+  flex-direction: row;
+  margin-top: 20px;
+  text-decoration: none;
+
+  button {
+    height: 4rem;
+    width: 8rem;
+    margin: 20px;
+    padding: 5px;
+    outline: none;
+    border: none;
+    border-radius: 20px;
+    background-color: rgb(137, 137, 255);
+    text-decoration: none;
+    cursor: pointer;
+  }
+`;
+
+const Last = styled.p`
+  color: white;
+  margin-left: -77rem;
+  font-size: 12px;
+`;
---
+++ src/hooks/UseState.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./useState.css";
+
+function UseState() {
+  const [count, setCount] = useState(() => {
+    console.log("run function");
+    return 0;
+  });
+
+  function decrement() {
+    setCount((prevCount) => prevCount - 1);
+    setCount((prevCount) => prevCount - 1);
+  }
+  function increment() {
+    setCount((nextCount) => nextCount + 1);
+    setCount((nextCount) => nextCount + 1);
+  }
+
+  return (
+    <div className="app">
+      <div className="left">
+        <button onClick={decrement}>Decrement by 2</button>
+        <button onClick={() => setCount(count - 1)}>Decrement by 1</button>
+      </div>
+      <h1>{count}</h1>
+      <div className="right">
+        <button onClick={() => setCount(count + 1)}>Increment by 1</button>
+        <button onClick={increment}>Increment by 2</button>
+      </div>
+      <p>Count set to: {count}</p>
+
+      <footer>
+        <span>
+          The useState() is a Hook that allows you to have state variables in
+          functional components . so basically useState is the ability to
+          encapsulate local state in a functional component. React has two types
+          of components, one is class components which are ES6 classes that
+          extend from React and the other is functional components. Class
+          components a Component and can have state and lifecycle methods: class
+          Message extends React. The useState hook is a special function that
+          takes the initial state as an argument and returns an array of two
+          entries. UseState encapsulate only singular value from the state, for
+          multiple state need to have useState calls.
+        </span>
+      </footer>
+    </div>
+  );
+}
+
+export default UseState;
